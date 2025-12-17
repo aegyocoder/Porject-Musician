280e9e0a1ed3d7ec6038c13264659234b6217a79
aegyocoder <juanda.caballerocruz@gmail.com>
2025-12-17 01:12:04 -0500

    Fix: include MVC_Music_Solution_Part 3 files (remove embedded repo)

---
+++ MVC_Music_Solution_Part 3/MVC_Music/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,145 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Music.Data
+{
+    public static class ApplicationDbInitializer
+    {
+        public static async void Initialize(IServiceProvider serviceProvider,
+            bool UseMigrations = true, bool SeedSampleData = true)
+        {
+            #region Prepare the Database
+            if (UseMigrations)
+            {
+                using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+                {
+                    try
+                    {
+                        //Create the database if it does not exist and apply the Migration
+                        context.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.GetBaseException().Message);
+                    }
+                }
+            }
+            #endregion
+
+            #region Seed Sample Data 
+            if (SeedSampleData)
+            {
+                //Create Roles
+                using (var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>())
+                {
+                    try
+                    {
+                        string[] roleNames = { "Admin", "Security", "Supervisor", "Staff" };
+
+                        IdentityResult roleResult;
+                        foreach (var roleName in roleNames)
+                        {
+                            var roleExist = await roleManager.RoleExistsAsync(roleName);
+                            if (!roleExist)
+                            {
+                                roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.GetBaseException().Message);
+                    }
+                }
+
+                //Create Users
+                using (var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>())
+                {
+                    try
+                    {
+                        string defaultPassword = "Pa55w@rd";
+
+                        if (userManager.FindByEmailAsync("admin@outlook.com").Result == null)
+                        {
+                            IdentityUser user = new IdentityUser
+                            {
+                                UserName = "admin@outlook.com",
+                                Email = "admin@outlook.com",
+                                EmailConfirmed = true
+                            };
+
+                            IdentityResult result = userManager.CreateAsync(user, defaultPassword).Result;
+
+                            if (result.Succeeded)
+                            {
+                                userManager.AddToRoleAsync(user, "Admin").Wait();
+                                userManager.AddToRoleAsync(user, "Security").Wait();
+                            }
+                        }
+
+                        if (userManager.FindByEmailAsync("security@outlook.com").Result == null)
+                        {
+                            IdentityUser user = new IdentityUser
+                            {
+                                UserName = "security@outlook.com",
+                                Email = "security@outlook.com",
+                                EmailConfirmed = true
+                            };
+                            IdentityResult result = userManager.CreateAsync(user, defaultPassword).Result;
+                            if (result.Succeeded)
+                            {
+                                userManager.AddToRoleAsync(user, "Security").Wait();
+                            }
+                        }
+
+                        if (userManager.FindByEmailAsync("supervisor@outlook.com").Result == null)
+                        {
+                            IdentityUser user = new IdentityUser
+                            {
+                                UserName = "supervisor@outlook.com",
+                                Email = "supervisor@outlook.com",
+                                EmailConfirmed = true
+                            };
+                            IdentityResult result = userManager.CreateAsync(user, defaultPassword).Result;
+                            if (result.Succeeded)
+                            {
+                                userManager.AddToRoleAsync(user, "Supervisor").Wait();
+                            }
+                        }
+                        if (userManager.FindByEmailAsync("staff@outlook.com").Result == null)
+                        {
+                            IdentityUser user = new IdentityUser
+                            {
+                                UserName = "staff@outlook.com",
+                                Email = "staff@outlook.com",
+                                EmailConfirmed = true
+                            };
+                            IdentityResult result = userManager.CreateAsync(user, defaultPassword).Result;
+                            if (result.Succeeded)
+                            {
+                                userManager.AddToRoleAsync(user, "Staff").Wait();
+                            }
+                        }
+                        if (userManager.FindByEmailAsync("user@outlook.com").Result == null)
+                        {
+                            IdentityUser user = new IdentityUser
+                            {
+                                UserName = "user@outlook.com",
+                                Email = "user@outlook.com",
+                                EmailConfirmed = true
+                            };
+                            IdentityResult result = userManager.CreateAsync(user, defaultPassword).Result;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.GetBaseException().Message);
+                    }
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ MVC_Music_Solution_Part 3/MVC_Music/ViewModels/PerformancesSummaryVM.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Music.ViewModels
+{
+    public class PerformancesSummaryVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Musician")]
+        public string summary
+        {
+            get
+            {
+                return First_Name
+                        + (string.IsNullOrEmpty(Middle_Name)
+                            ? " "
+                            : " " + ((char)Middle_Name[0]).ToString().ToUpper() + ". ")
+                        + Last_Name;
+            }
+        }
+        public string First_Name { get; set; } = "";
+        public string Last_Name { get; set; } = "";
+        public string Middle_Name { get; set; } = "";
+
+        [Display(Name = "Average fee paid")]
+        [DataType(DataType.Currency)]
+        public double Average_Fee { get; set; }
+
+        [Display(Name = "Highest fee paid")]
+        [DataType(DataType.Currency)]
+        public double Highest_Fee { get; set; }
+
+        [Display(Name ="Fewes fee paid")]
+        [DataType(DataType.Currency)]
+        public double Fewest_Fee { get; set; }
+
+        [Display(Name = "Number of Performances")]
+        public int? Total_Of_Performances { get; set; }
+    }
+}
